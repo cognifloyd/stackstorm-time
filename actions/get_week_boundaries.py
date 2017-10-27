679a961ad020bd3074a87dc608e38eb364d7087c
Kirill Izotov <enykeev@stackstorm.com>
2017-10-27 14:55:05 +0700

    Remove deprecated st2actions import

--- actions/get_week_boundaries.py
+++ actions/get_week_boundaries.py
@@ -1,7 +1,7 @@
 import arrow
 
 from st2common.util import isotime
-from st2actions.runners.pythonrunner import Action
+from st2common.runners.base_action import Action
 
 
 class GetWeekBoundariesTimestampsAction(Action):
